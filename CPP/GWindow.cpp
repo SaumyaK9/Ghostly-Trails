033bc48053701680da4f1dd843ce848ab8a025ee
SaumyaK9 <135534345+SaumyaK9@users.noreply.github.com>
2024-12-08 12:17:53 +0530

    Create GWindow.cpp

---
+++ CPP/GWindow.cpp
@@ -0,0 +1,107 @@
+#include "GWindow.h"
+
+void GWindow::config()
+{
+	window.create(sf::VideoMode(width, height), "GHOSTLY TRAILS", sf::Style::Close);
+	window.setPosition(sf::Vector2i(0, 0));
+	window.setFramerateLimit(60);
+
+	// setGameIcon();
+}
+
+void GWindow::setGameIcon()
+{
+	sf::Image icon;
+	icon = FileManager::openImage("D:/code/projects/Territory Wars/resources/Logo.png");
+	window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+}
+
+GWindow::GWindow()
+{
+	config();
+}
+
+sf::RenderWindow& GWindow::getWindow() { return window; }
+
+void GWindow::deleteMenu()
+{
+	menu.release();
+}
+
+void GWindow::setMenu()
+{
+	menu = std::make_unique<Menu>();
+	menu->checkForDemand(this->demandCredits);
+	demandCredits = false;
+}
+
+std::array<bool, 3> GWindow::displayMenu()
+{
+	std::array<bool, 3> choices{ 0,0,0 };
+	while (window.isOpen() && menu->getStatus() != 0)
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+			if (event.type == sf::Event::Closed)
+				window.close();
+
+		choices = menu->menuLogic(window);
+
+		window.clear();
+		menu->displayMenu(window);
+		window.display();
+
+	}
+
+	menu.reset();
+	return choices;
+}
+
+void GWindow::setInfoPanel(int _x, int _y, int _w, int _h)
+{
+	iP = std::make_unique<InfoPanel>(_x, _y, _w, _h);
+	iP->setNewInfo(0, 0, 0.0, 0.0);
+}
+
+void GWindow::displayInfoPanel(int level, int hp, double percent, double percent_needed)
+{
+	iP->setNewInfo(level, hp, percent, percent_needed);
+	iP->drawInfo(this->window);
+}
+
+void GWindow::setVictoryBox()
+{
+	tB = std::make_unique<TextBox>(width / 2 - 600, height / 2 - 350, 1200, 500, "Congratulations!", "You have won the game :D", "To credits");
+	tB->turnFrame(true);
+	tB->fillFrame(true);
+}
+
+void GWindow::setDefeatBox()
+{
+	tB = std::make_unique<TextBox>(width / 2 - 400, height / 2 - 350, 800, 500, "You Lose!", "press back to return to menu");
+	tB->turnFrame(true);
+}
+void GWindow::setPauseBox()
+{
+	tB = std::make_unique<TextBox>(width / 2 - 350, height / 2 - 300, 700, 400, "Pause", " ", "Back", "Save & Quit");
+	tB->turnFrame(false);
+}
+
+std::pair<bool, int> GWindow::getTextBoxResponse()
+{
+	std::pair respond = tB->waitForButton(this->window);
+	if (!respond.first)
+	{
+		tB.release();
+
+		return std::make_pair(false, respond.second);
+	}
+	return std::make_pair(true, respond.second);
+
+
+}
+
+void GWindow::displayTextBox()
+{
+	tB->displayTextBox(this->window);
+}
